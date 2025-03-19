5a08f38985d320d97282d839ff0757280d4f77a6
Rawann0m <raawannm@gmail.com>
2025-03-19 13:11:03 +0300

    Initial Commit

---
+++ AlamofireAPITask/AlamofireAPITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireAPITaskApp.swift
+//  AlamofireAPITask
+//
+//  Created by Rawan on 19/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireAPITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
